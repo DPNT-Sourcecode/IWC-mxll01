b6294dcdf284d8d7ca82a53a52bf6beb6b27ad69
sbx_user1051 <sbx_user1051@169.254.70.25>
2026-01-11 14:18:40 +0000

    Sun Jan 11 14:18:40 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -63,3 +63,14 @@ def test_legacy_age() -> None:
         call_age().expect(300),
     ])
 
+def test_legacy_time_sensitive_bank_statements() -> None:
+    run_queue([
+        call_enqueue("id_verification", 1, iso_ts()).expect(1),
+        call_enqueue("bank_statements", 2, iso_ts(delta_minutes=1)).expect(2),
+        call_enqueue("companies_house", 3, iso_ts(delta_minutes=7)).expect(3),
+        call_dequeue().expect("id_verification", 1),
+        call_dequeue().expect("bank_statements", 2),
+        call_dequeue().expect("companies_house", 3),
+    ])
+
+
