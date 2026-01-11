a6abc590a5a56145d1bcc3c2a07264270b14e447
sbx_user1051 <sbx_user1051@169.254.70.25>
2026-01-11 16:23:19 +0000

    Sun Jan 11 16:23:19 UTC 2026

--- lib/solutions/IWC/queue_solution_legacy.py
+++ lib/solutions/IWC/queue_solution_legacy.py
@@ -126,11 +126,10 @@ class Queue:
             priority_timestamps[user_id] = earliest_timestamp
             task_count[user_id] = len(user_tasks)
 
-        oldest_timestamp = max(self._timestamp_for_task(task) for task in self._queue)
-
+        oldest_timestamp = None
         for task in self._queue:
-            # if oldest_timestamp is None or self._timestamp_for_task(task) > oldest_timestamp:
-            #     oldest_timestamp = self._timestamp_for_task(task)
+            if oldest_timestamp is None or self._timestamp_for_task(task) > oldest_timestamp:
+                oldest_timestamp = self._timestamp_for_task(task)
             metadata = task.metadata
             current_earliest = metadata.get("group_earliest_timestamp", MAX_TIMESTAMP)
             raw_priority = metadata.get("priority")
@@ -144,8 +143,6 @@ class Queue:
                 metadata["group_earliest_timestamp"] = MAX_TIMESTAMP
                 if task_count[task.user_id] >= 3:
                     metadata["group_earliest_timestamp"] = priority_timestamps[task.user_id]
-                    metadata["priority"] = Priority.HIGH
-                elif is_bank_statements and oldest_timestamp is not None and (oldest_timestamp - self._timestamp_for_task(task)).seconds >= 300:
                     metadata["priority"] = Priority.HIGH
                 else:
                     metadata["priority"] = Priority.NORMAL
@@ -165,7 +162,7 @@ class Queue:
                     return (priority, group_timestamp, MAX_TIMESTAMP, 1)
                 else:
                     bank_priority = 0
-                    # priority = Priority.HIGH
+                    #priority = 0
 
             return (priority, group_timestamp, task_timestamp, bank_priority)
 
@@ -282,3 +279,4 @@ async def queue_worker():
         logger.info(f"Finished task: {task}")
 ```
 """
+
